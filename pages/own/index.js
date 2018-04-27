3046ee8742a9dbc42b8d5302290e7c25b1216a38
HyOner <2582458676@qq.com>
2018-04-27 23:27:20 +0800

    collect business Done

--- pages/own/index.js
+++ pages/own/index.js
@@ -18,13 +18,14 @@ Page({
   onLoad: function (options) {
     let userInfo = wx.getStorageSync('userInfo')
     this.setData({
-      userInfo: userInfo,
+      userInfo: userInfo
     })
     let datas = [];
     datas['key'] = 'postList'
     datas['s'] = 'App.Main_Set.GetList'
 
     util.http(app.globalData.okayApiHost, 2, datas, this.poccessData);
+
   },
 
   onReady: function (options) {
@@ -40,58 +41,27 @@ Page({
   },
 
   poccessData(e) {
-    //数据处理函数:改变时间显示格式, 添加item收藏状态数据
-    let  postCollected = wx.getStorageSync('postCollected') || {}
     //遍历每个post数据的add_time,将它们转化成'几天前' 
     for (let i = 0, length = e.data.items.length; i < length; i++) {
       let date = e.data.items[i].add_time
       //调用时间处理函数进行相关类型转换
-      e.data.items[i].add_time = this.poccessDate(date);
-      e.data.items[i]['postCollected'] = postCollected[i+1];
+      e.data.items[i].add_time = this.poccessDate(date)
     }
+
     this.setData({
       postData: e.data.items
     })
-  },
 
+  },
   poccessDate(date) {
     if ((typeof date) === 'string') {
       //将字符串日期转换为日期格式
       let newDate = new Date(Date.parse(date.replace(/-/g, "/")))
       //将日期格式转换为时间戳
-      let fixedDate = newDate.getTime() 
+      let fDate = newDate.getTime()
       //调用时间格式化函数, 返回时间显示为'几天前'
-      return util.dateStr(fixedDate)
- 
+      return util.dateStr(fDate)
     }
-  },
-
-  collectPost(e) {
-    let that = this;
-    let postid = e.currentTarget.dataset.postid;
-    //让收藏数加1, 存到要发送的数据中
-    let collected_count = Number(that.data.postData[postid - 1].data.collected_count) + 1;
-    let data = that.data.postData[postid - 1].data;
-    data.collected_count = collected_count;
-    let datas = {
-      s: 'App.Main_Set.Update',
-      id: postid,
-      data: JSON.stringify(data)
-    }
-    // util.http(app.globalData.okayApiHost, 2, datas);
-    
-    //点击收藏后,将收藏状态取反,并存入页面data和缓存中
-    let postCollected = wx.getStorageSync('postCollected') || {}
-    if (postCollected[postid]) {
-      postCollected[postid] = !postCollected[postid];
-    } else {
-      postCollected[postid] = true
-    }
-
-     that.data.postData[postid - 1]['postCollected'] = postCollected[postid];
-    that.setData({ postData: that.data.postData})
-    
-    wx.setStorageSync('postCollected', postCollected)
   },
 
 
@@ -132,9 +102,7 @@ Page({
 
     let that = this;
     let postid = e.currentTarget.dataset.postid;
-    this.setData({
-      postid
-    });
+    this.setData({ postid });
 
     //调用querySelector查询当前点击的帖子高度
     let id = this.data.postData[postid - 1].id;
@@ -143,9 +111,7 @@ Page({
     query.select(queryid).boundingClientRect()
     //动态设定动画, 保证每个盒子展开正常
     query.exec(function (res) {
-      that.setData({
-        height: res[0].height
-      })
+      that.setData({ height: res[0].height })
       let height = that.data.height + 299;
       that.animation.height(height).step()
       that.setData({
@@ -156,18 +122,14 @@ Page({
 
   onTapUp(e) {
     let postid = e.currentTarget.dataset.postid;
-    this.setData({
-      postid:postid
-    });
+    this.setData({ postid });
     this.animation.height(76).step({
       duration: 500
     })
     this.setData({
       animationData: this.animation.export()
     })
-    this.setData({
-      postid: null
-    });
+    this.setData({ postid: null });
   }
 
 })
--- utils/util.js
+++ utils/util.js
@@ -50,8 +50,7 @@ function dateStr(date) {
     //获取js 时间戳
     var time = new Date().getTime();
     //去掉 js 时间戳后三位，与php 时间戳保持一致  
-    time = parseInt(((time - time % 1000) - date) / 1000);;
-
+    time = parseInt(((time - time % 1000) - date) / 1000);
     //存储转换值 
     var s;
     if (time < 60 * 10) { //十分钟内
@@ -70,7 +69,7 @@ function dateStr(date) {
         return s + "天前";
     } else {
         //超过3天
-        var date = new Date(parseInt(date) * 1000);
+        var date = new Date(parseInt(date));
         return date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
     }
 }
